a1c88a7300377c8a8619e91a638c1fd542bca8f1
DDK <ddkgeorgetown@gmail.com>
2019-02-27 18:56:23 -1000

    init

---
+++ 1 Arrays/meetingRoomsI.js
@@ -0,0 +1,8 @@
+// https://www.programcreek.com/2014/07/leetcode-meeting-rooms-java/
+
+/*
+Given an array of meeting time intervals consisting of start and end times [ [s1,e1], [s2,e2],...] (si < ei),
+determine if a person could attend all meetings.
+
+For example, Given [ [0, 30], [5, 10], [15, 20] ], return false.
+*/
