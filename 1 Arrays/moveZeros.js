ac3b590bdc85f4c188fecfdd066816661c70d285
DDK <ddkgeorgetown@gmail.com>
2019-03-07 21:49:23 -1000

    COMPLETE

--- 1 Arrays/moveZeros.js
+++ 1 Arrays/moveZeros.js
@@ -8,3 +8,16 @@ Example:
 Input: [0,1,0,3,12]
 Output: [1,3,12,0,0]
 */
+
+const moveZero = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 0) {
+      array.splice(i, 1)
+      array.push(0)
+    }
+  }
+  return array
+}
+
+let test = [0, 1, 0, 3, 12]
+console.log(moveZero(test));
