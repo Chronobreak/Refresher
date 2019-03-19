57f16f8924d7543b31ce07fc9853d03876171259
DDK <ddkgeorgetown@gmail.com>
2019-03-18 22:58:16 -0400

    COMPLETE - O (m * n) where m is max length and n is array length

--- 1 Arrays/[COMPLETE] alienDictionary.js
+++ 1 Arrays/[COMPLETE] alienDictionary.js
@@ -24,22 +24,38 @@ The correct order is: "zx".
 
 const decode = (array) => {
   let max = 0;
+  let hash = {};
   let result = [];
   // loop through array[i][0] to compare lengths
   // maybe just push the letters into the array like so:
     // if (!result.includes(array[i][index])) result.push(array[i][index])
+
+  // obtain the max length of a string so you know how many times to loop
   for (let i = 0; i < array.length; i++) {
     if (array[i].length > max) {
       max = array[i].length;
     }
   }
 
+  // loop through each 
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < max; j++) {
-      if (array[i][j]) {
-        
+      if (hash[array[j][i]]) {
+        continue
+      } else if (array[j][i] !== undefined) {
+        hash[array[j][i]] = 1
+        result.push(array[j][i])
       }
     }
-    
   }
+  return result.join('');
 }
+
+let test = [
+  "wrt",
+  "wrf",
+  "er",
+  "ett",
+  "rftt"
+]
+console.log("Should equal wertf: ", decode(test));
