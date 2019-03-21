462be85484da36d7e11c169741ff76cbf0e4e33e
DDK <ddkgeorgetown@gmail.com>
2019-03-20 23:35:13 -0400

    init

--- Helpers.js
+++ Helpers.js
@@ -4,6 +4,18 @@ Use this file if you need quick examples
 
 
 // Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 
 
 // Tree
